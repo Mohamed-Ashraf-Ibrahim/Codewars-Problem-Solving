bc8bd20d6579b4c6f0f89111e20f861c345e8aee
Mohamed Ashraf <146873948+Mohamed-Ashraf-Ibrahim@users.noreply.github.com>
2023-10-29 21:40:13 +0200

    Update ES6 string addition.js

--- ES6 string addition.js
+++ ES6 string addition.js
@@ -1,3 +1,11 @@
+/*
+ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, 
+there must be a space between the two strings.
++ , .concat() & .join() will not be allowed in this exercise. I have also removed some other methods that can be used to cheat!
+
+If one of the arguments is a number your code must coerce it into being a string.
+*/
+
 function joinStrings(string1, string2) {
   // code here
   return `${string1} ${string2}`;
