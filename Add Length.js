7b1626ac2a30e2933b936dd7b766b1a9b79de551
Mohamed Ashraf <146873948+Mohamed-Ashraf-Ibrahim@users.noreply.github.com>
2023-11-20 06:32:28 +0200

    Update Add Length.js

--- Add Length.js
+++ Add Length.js
@@ -1,8 +1,5 @@
 function addLength(str) {
   //start-here
-  const words = str.split(" ");
-  // Map over the array of words and create a new array (len) where each element
-  // consists of the original word followed by its length separated by a space.
-  const len = words.map((word) => `${word} ${word.length}`);
-  return len;
+  const words = str.split(" ").map((word) => `${word} ${word.length}`);
+  return words;
 }
