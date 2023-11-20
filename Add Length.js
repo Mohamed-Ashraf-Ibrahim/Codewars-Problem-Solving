189a4ffa4cb1bdb59080f99b191227be47c80994
Mohamed Ashraf <146873948+Mohamed-Ashraf-Ibrahim@users.noreply.github.com>
2023-11-20 06:34:17 +0200

    Update Add Length.js

--- Add Length.js
+++ Add Length.js
@@ -1,5 +1,4 @@
 function addLength(str) {
   //start-here
-  const words = str.split(" ").map((word) => `${word} ${word.length}`);
-  return words;
+  return str.split(" ").map((word) => `${word} ${word.length}`);
 }
