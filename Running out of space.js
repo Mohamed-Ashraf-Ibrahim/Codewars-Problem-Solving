23b72ac0c9a3830e5708bced2108582c74803ac0
Mohamed Ashraf <mohamed.ash.1523@gmail.com>
2024-03-17 15:24:52 +0200

    A new problem has been solved

--- Running out of space.js
+++ Running out of space.js
@@ -1,3 +1,8 @@
+/*
+Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
+For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
+*/
+
 function spacey(array) {
   const result = [];
   let concatenated = "";
@@ -8,5 +13,3 @@ function spacey(array) {
   }
   return result;
 }
-
-console.log(spacey(["i", "have", "no", "space"]));
