0c5ce2c197efe20ebe987f8b78e0a9eb17632030
Mohamed Ashraf <mohamed.ash.1523@gmail.com>
2024-03-20 00:07:44 +0200

    A new problem has been solved

--- Highest and Lowest.js
+++ Highest and Lowest.js
@@ -1,9 +1,12 @@
+/*
+https://www.codewars.com/kata/554b4ac871d6813a03000035
+*/
 function highAndLow(numbers) {
   // ...
   numbers = numbers.split(" ").map(Number);
   let max = numbers[0];
   let min = numbers[0];
-  
+
   for (let i = 1; i < numbers.length; i++) {
     if (numbers[i] < min) {
       min = numbers[i];
