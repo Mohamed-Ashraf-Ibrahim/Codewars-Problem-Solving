8e07f8d0f290d8a2544b496028099a9a78a11ef1
Mohamed Ashraf <mohamed.ash.1523@gmail.com>
2024-04-26 21:28:13 +0300

    A new problem has been added

---
+++ Count by X.js
@@ -0,0 +1,8 @@
+/* https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/javascript*/
+function countBy(x, n) {
+  let z = [];
+  for (let i = 1; i <= n; i++) {
+    z.push(x * i);
+  }
+  return z;
+}
