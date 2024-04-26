d3b99f5c71d7c6a492895c343d08eb10ea12ec2e
Mohamed Ashraf <mohamed.ash.1523@gmail.com>
2024-04-26 21:29:13 +0300

    A new problem has been solved

---
+++ CSV representation of array.js
@@ -0,0 +1,12 @@
+/* https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036/train/javascript */
+function toCsvText(array) {
+  // good luck
+  let result = "";
+  for (let i = 0; i < array.length; i++) {
+    result += array[i].join(",") + "\n";
+  }
+  if (result.length > 1) {
+    result = result.slice(0, -1);
+  }
+  return result;
+}
