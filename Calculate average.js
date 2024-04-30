d16a23dec12147755c836142c4b3153657f081cb
Mohamed Ashraf <mohamed.ash.1523@gmail.com>
2024-04-30 20:44:11 +0300

    A new problem has been solved

--- Calculate average.js
+++ Calculate average.js
@@ -1,7 +1,9 @@
 // https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/javascript
 function findAverage(array) {
   // your code here
-  array.length === 0 ? 0 : false;
+  if (array.length === 0) {
+    return 0;
+  }
   let sum = array.reduce((acc, num) => acc + num, 0);
   const avg = sum / array.length;
   return avg;
